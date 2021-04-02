512a9f0ee3cf5b53aec2374e9f387396f665ee3e
unknown <sdsakshi13@gmail.com>
2021-04-02 21:13:42 +0530

    initial commit

---
+++ youtubers/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Youtuber
+# Register your models here.
+
+class YtAdmin(admin.ModelAdmin):
+    
+    list_display = ('id', 'name', 'subs_count', 'is_featured')
+    search_fields = ('name', 'camera_type')
+    list_filter = ('city', 'camera_type')
+    list_display_links = ('id', 'name')
+    list_editable = ('is_featured',)
+
+
+admin.site.register(Youtuber, YtAdmin )
